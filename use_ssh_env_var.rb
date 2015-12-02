b3a59aff45372b32b1cb20edd4a81ce13803af6c
Brian Gates <core.safecrlf>
2015-12-02 14:45:56 -0500

    some config tweak must work

--- use_ssh_env_var.rb
+++ use_ssh_env_var.rb
@@ -1,11 +1,10 @@
 require 'tempfile'
 file = Tempfile.new('pem')
 begin
-  puts "env set: #{ENV['AWS_IP']}, #{ENV['AWS_DNS']}, and"
-  puts ENV['RETHINKDB_SSH']
   file.write(ENV['RETHINKDB_SSH'])
   puts 'file was written'
   `nohup ssh -n -i #{file.path} -o StrictHostKeyChecking=no -N ubuntu@#{ENV['AWS_DNS']} -p 1000 -L 127.0.0.1:28015:#{ENV['AWS_IP']}:28015`
+  puts 'please'
 ensure
   file.close
   file.unlink
