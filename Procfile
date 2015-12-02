c7a221d442c2aa2e61b951fe5fc1952436eb4f6f
Brian Gates <core.safecrlf>
2015-12-02 14:23:45 -0500

    try to run Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: /bin/bash -c 'ruby use_ssh_env_var.rb;  bundle exec rails server -p $PORT'
+web: /bin/bash -c 'ruby use_ssh_env_var.rb & bundle exec rails server -p $PORT'
--- use_ssh_env_var.rb
+++ use_ssh_env_var.rb
@@ -1,8 +1,11 @@
 require 'tempfile'
 file = Tempfile.new('pem')
 begin
+  puts "env set: #{ENV['AWS_IP']}, #{ENV['AWS_DNS']}, and"
+  puts ENV['RETHINKDB_SSH']
   file.write(ENV['RETHINKDB_SSH'])
-  `(nohup ssh -n -i #{file.path} -o StrictHostKeyChecking=no -N ubuntu@#{ENV['AWS_DNS']} -p 1000 -L 127.0.0.1:28015:#{ENV['AWS_IP']}:28015 &)`
+  puts 'file was written'
+  `nohup ssh -n -i #{file.path} -o StrictHostKeyChecking=no -N ubuntu@#{ENV['AWS_DNS']} -p 1000 -L 127.0.0.1:28015:#{ENV['AWS_IP']}:28015`
 ensure
   file.close
   file.unlink
