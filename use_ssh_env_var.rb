2a7f92c32a88804d49ca589d8b567150060f8f9e
Brian Gates <core.safecrlf>
2015-12-02 22:24:27 -0500

    attempt debug sshd

--- use_ssh_env_var.rb
+++ use_ssh_env_var.rb
@@ -10,8 +10,8 @@ begin
     f.puts "ec2-52-23-215-146.compute-1.amazonaws.com,52.23.215.146 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAPOYlX3f1fsnYAmYJSPPwJIHHyrkVqSR9tfhKiF1SnrEacPdLGKfPQhJBrB1L4j1HHCuHJoeWQZ1uH5GV942LRQT/HZ3mOBLy6CnRRDAa4dLl+1+ILzNp90pEKo2jIbjea8Iqp2JlzX6zmqO8oMJrls7qr6zLccdHvXzIrtAmCnPgc20O5Uni4B0UraXNLBBsL7ddnxadf7xPk/dKhxC1c6KCAATrQlAsROy/5Yw3msBzkf0IttSB9vTH2PQRZjfNTH/wn/7NLMdYZ3YaaN7nQ4nrCZ3LqG2dm3WB/8xCXSb21v3Y2kCMyFL7CjlBlUyRzxOMMhG3Vm6pw/5dRsU/"
   end
   puts 'about to run following:'
-  puts "ssh ubuntu@#{ENV['AWS_DNS']} -vvv -f -i #{file.path} -o StrictHostKeyChecking=no -N -L 28015:#{ENV['AWS_IP']}:28015"
-  `ssh -vvv -f -i #{file.path} -o StrictHostKeyChecking=no -N ubuntu@#{ENV['AWS_DNS']}  -L 28015:#{ENV['AWS_IP']}:28015`
+  puts "ssh ubuntu@#{ENV['AWS_DNS']} -vvv -f -i #{file.path} -o StrictHostKeyChecking=no -p 2222 -N -L 28015:#{ENV['AWS_IP']}:28015"
+  `ssh -vvv -f -i #{file.path} -o StrictHostKeyChecking=no -N ubuntu@#{ENV['AWS_DNS']}  -p 2222 -L 28015:#{ENV['AWS_IP']}:28015`
   puts 'please'
 ensure
   file.close
